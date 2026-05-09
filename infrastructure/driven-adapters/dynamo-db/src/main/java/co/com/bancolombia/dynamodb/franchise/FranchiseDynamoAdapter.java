73af773e6da1907b0674214c42750ad6b902a345
AlejoSilva97 <alejandrosilva1897@gmail.com>
2026-05-09 11:13:43 -0500

    refactor(dynamo-adapter): remove unused import

--- infrastructure/driven-adapters/dynamo-db/src/main/java/co/com/bancolombia/dynamodb/franchise/FranchiseDynamoAdapter.java
+++ infrastructure/driven-adapters/dynamo-db/src/main/java/co/com/bancolombia/dynamodb/franchise/FranchiseDynamoAdapter.java
@@ -8,7 +8,6 @@ import io.github.resilience4j.circuitbreaker.CircuitBreakerRegistry;
 import io.github.resilience4j.reactor.circuitbreaker.operator.CircuitBreakerOperator;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Repository;
-import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 import software.amazon.awssdk.enhanced.dynamodb.DynamoDbAsyncTable;
 import software.amazon.awssdk.enhanced.dynamodb.DynamoDbEnhancedAsyncClient;
